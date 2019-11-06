990a4d79ea1d60cfe0859aaf07a4e3935acff74d
Andy Feidje Joseph <ingandy75@gmail.com>
2019-11-06 00:22:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,19 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  scope '/admin', module: 'admin' do
+    resources :stats, only: [:index]
+  end
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
+  get '/admin/stats', to: 'stats#index'
+  get '/admin/authors/new', to: 'authors#new'
+  get '/admin/authors/delete', to: 'authors#delete'
+  get '/admin/authors/create', to: 'authors#create'
+  get '/admin/comments/moderate', to: 'comments#moderate'
+
 end
